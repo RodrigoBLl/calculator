df7d8f73f75c3f6a813e4058d0d42779d86d88dd
Rodrigo Llanas <rodrigollanas202@hotmail.com>
2024-05-11 15:49:08 -0600

    calculator

--- calculator.js
+++ calculator.js
@@ -8,11 +8,23 @@ function appendNumber(number) {
 function calculate() {
     let result;
     try {
+
         result = eval(currentInput); // Utiliza la función eval() para evaluar la expresión
         document.getElementById('display').value = result;
     } catch (error) {
         document.getElementById('display').value = 'Error';
     }
+}
+
+function squareRoot() {
+    let input = document.getElementById('display').value;
+    let result = Math.sqrt(parseFloat(input)); // Calcula la raíz cuadrada del número actual
+    document.getElementById('display').value = result;
+}
+
+function deleteCharacter(){
+    currentInput = currentInput.slice(0, -1);
+    document.getElementById('display').value = currentInput;
 }
 
 function clearDisplay() {
